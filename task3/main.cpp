338107b851cbf69d9baea300c78a6b7331f23c37
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2022-10-20 02:26:27 +0300

    chanched task 3

--- task3/main.cpp
+++ task3/main.cpp
@@ -15,26 +15,27 @@ struct pi_CONST {
 
 int main(int argc, char** argv) {
 
-#ifdef _WIN32
-	setlocale(LC_ALL, "ru");
-#endif
-
-	std::vector<int> angles = { 30, 60, 90 };
+	std::vector<double> angles = { 30, 60, 90 };
 
 	std::string deg_to_rad_str = " * " + std::to_string(pi.rad) + " / " + std::to_string(pi.deg);
  
 	std::cout << "[Входные данные]: ";
 
-	std::string result = std::accumulate(std::next(angles.begin()), angles.end(), std::to_string(angles[0]) + deg_to_rad_str,
+	std::string result = std::accumulate(std::next(angles.begin()), angles.end(), std::to_string(static_cast<int>(angles[0])) + deg_to_rad_str,
 		[deg_to_rad_str](std::string angle1, int angle2) {
 			return angle1 + ", " + std::to_string(angle2) + deg_to_rad_str;
-		});
+		}) 
+		+ " // перевод из градусов в радианы";
 
-	std::cout << result << " // перевод из градусов в радианы" << std::endl;
+	std::cout << result  << std::endl;
+
+	for (auto& angle : angles) {
+		angle *= (pi.rad / pi.deg);
+	}
 
 	std::vector<std::function<void(double)>> functions {
-		[](double angle) { std::cout << "sin: " << std::sin(angle * (pi.rad / pi.deg)) << " "; },
-		[](double angle) { std::cout << "cos: " << std::cos(angle * (pi.rad / pi.deg)) << " "; }
+		[](double angle) { std::cout << "sin: " << std::sin(angle) << " "; },
+		[](double angle) { std::cout << "cos: " << std::cos(angle) << " "; }
 	};
 
 	std::cout << "[Выходные данные]:" << std::endl;
