fe7c58663c2d8340f93c97b85fbe2a887a76e619
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2022-10-20 01:54:05 +0300

    solutions added

---
+++ task1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void printContainer(const T& container) {
+	
+	for (const auto& elements : container) {
+		std::cout << elements << " ";
+	}
+
+	std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+
+#ifdef _WIN32
+	setlocale(LC_ALL, "ru");
+#endif
+
+	std::vector<int> v = { 4, 7, 9, 14, 12 };
+
+	printContainer(v);
+
+	std::for_each(v.begin(), v.end(), 
+		[](auto& value) {
+
+			if (value % 2 != 0) { value *= 3; }
+
+		});
+
+	printContainer(v);
+
+	return 0;
+}
---
+++ task2/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <vector>
+#include <variant>
+
+template<typename T>
+void printContainer(const T& container) {
+
+	for (const auto& elements : container) {
+		std::cout << elements << " ";
+	}
+
+	std::cout << std::endl;
+}
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+
+	return result;
+}
+
+int main(int argc, char** argv) {
+
+	auto result_ = get_variant();
+
+	if (std::holds_alternative<int>(result_)) {
+		auto result_int = std::get<int>(result_);
+		std::cout << result_int*2 << std::endl;
+	}
+	else if (std::holds_alternative<std::string>(result_)) {
+		auto result_string = std::get<std::string>(result_);
+		std::cout << result_string << std::endl;
+	}
+	else if (std::holds_alternative<std::vector<int>>(result_)) {
+		auto result_vector = std::get<std::vector<int>>(result_);
+		printContainer(result_vector);
+	}
+
+	return 0;
+}
