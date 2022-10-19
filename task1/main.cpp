b3232c3225560921f98520c7a81546e7ee3dcfd8
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2022-10-20 01:59:35 +0300

    task1 design

--- task1/main.cpp
+++ task1/main.cpp
@@ -6,11 +6,9 @@
 
 template<typename T>
 void printContainer(const T& container) {
-	
 	for (const auto& elements : container) {
 		std::cout << elements << " ";
 	}
-
 	std::cout << std::endl;
 }
 
@@ -21,16 +19,10 @@ int main(int argc, char** argv) {
 #endif
 
 	std::vector<int> v = { 4, 7, 9, 14, 12 };
-
 	printContainer(v);
 
 	std::for_each(v.begin(), v.end(), 
-		[](auto& value) {
-
-			if (value % 2 != 0) { value *= 3; }
-
-		});
-
+		[](auto& value) { if (value % 2 != 0) { value *= 3; } });
 	printContainer(v);
 
 	return 0;
